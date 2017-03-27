42ee41434c089f370b4df5105e7c3a3f3828de9f
ben rudgers <ben.rudgers@gmail.com>
2017-03-27 16:56:27 -0500

    still working out packaging for tests

--- test/eliza.test.lisp
+++ test/eliza.test.lisp
@@ -2,10 +2,15 @@
 
 (defpackage :eliza-test
   (:use :common-lisp
+        :asdf
         :eliza
         :lisp-unit))
 
 (in-package :eliza-test)
+
+(defsystem eliza-test
+    :serial t
+    :components ((:file "eliza.test")))
 
 (define-test test-one
     (assert-equal 5 5))
